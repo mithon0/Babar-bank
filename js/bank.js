4ee13e584769d1d1c64ecd6d767a8b701bbc7741
mithon0 <mithonhossen7244@gmail.com>
2023-02-14 11:00:49 +0600

    fnal commit

--- js/bank.js
+++ js/bank.js
@@ -2,13 +2,51 @@
 document.getElementById('diposite-btn').addEventListener('click',function(){
     // console.log('clicked')
     const dipositeField =document.getElementById('diposite-input');
-    const dipositeAmount =dipositeField.value;
+    const dipositeAmountString =dipositeField.value;
+    const dipositeAmount = parseFloat(dipositeAmountString);
+
     // console.log(dipositeAmount);
     const dipositeElementField =document.getElementById('diposite-output');
-    const dipositeTotal =dipositeElementField.innerText;
-    dipositeElementField.innerText = dipositeAmount;
+    const dipositeTotalstring =dipositeElementField.innerText;
+    const dipositeTotal = parseFloat(dipositeTotalstring);
 
-    // clear stap 
+    const currentDipositeTotal = dipositeAmount + dipositeTotal ;
+    dipositeElementField.innerText = currentDipositeTotal;
+    // console.log(typeof dipositeAmount);
+
+// ballence-----------------------------------------------------------
+    const ballanceElement = document.getElementById('ballance-amount');
+    const ballaneTotalString = ballanceElement.innerText;
+    const ballanceTotal =parseFloat(ballaneTotalString);
+
+    const totalBllance = ballanceTotal + dipositeAmount;
+    ballanceElement.innerText = totalBllance;
+
     dipositeField.value ='';
 
+})
+// withdraw----------------------------------------------------------------
+document.getElementById('Withdraw-btn').addEventListener('click',function(){
+        // console.log('get the button')
+        const withdrawElement =document.getElementById('withdraw-input');
+        const withdrawTotalString =withdrawElement.value;
+        const withdrawTotal =parseFloat(withdrawTotalString);
+
+
+        const withdrawElementField =document.getElementById('withdraw-output');
+            const withdrawElementTotalString =withdrawElementField.innerText;
+            const withdrawElementTotal =parseFloat(withdrawElementTotalString);
+
+            const totalWithdraw = withdrawTotal + withdrawElementTotal;
+            withdrawElementField.innerText =totalWithdraw;
+            withdrawElement.value = '';
+
+            const finalBallance =document.getElementById('ballance-amount');
+            const getFinalBallaneString =finalBallance.innerText;
+            const finalBallanceTotal =parseFloat(getFinalBallaneString);
+            const ballanceAfterWithdraw = finalBallanceTotal - withdrawTotal;
+            // set the final ballance 
+            finalBallance.innerText = ballanceAfterWithdraw;
+
+        
 })
